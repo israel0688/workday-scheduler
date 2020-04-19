c0d36cba650e7a606e0713dfd8dd7f26018e0894
Israel Berlanga <israel0688@hotmail.com>
2020-04-19 14:38:18 -0500

    semi working backgroun color

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,37 @@
 
+var hour = moment().format('h');
+var nowHour = document.getElementById('theHour')
+var color = document.getElementById('midColumn')
 //Display todays date in header
-(function()
+function headingDate()
 {
   // instantiate a moment object
-  var NowDate = moment().format("MMMM Do YYYY");
+ var NowDate = moment().format("MMMM Do YYYY");
   
-
-  
+ 
   // display value of moment object in #currentDay div
   var eDisplayDate = document.getElementById('currentDay');
   eDisplayDate.innerHTML = NowDate;
   
 
-})();
+}
+headingDate();
+
+
+function backgroundColor()
+{
+  
+
+    if (hour=nowHour)
+    color.style.backgroundColor = "lightblue";
+
+    else
+    color.style.backgroundColor = "gray";
+        
+  
+}
+backgroundColor();
+
+
+
+
